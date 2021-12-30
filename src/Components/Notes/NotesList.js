e8db09ef7560f53ff2ca92cfee186e36f87b39c4
Dave Fredkove <david.fredkove@target.com>
2021-12-30 13:35:23 -0600

    add grid layout for notes list

--- src/Components/Notes/NotesList.js
+++ src/Components/Notes/NotesList.js
@@ -1,21 +1,43 @@
 import Note from "./Note"
 import AddNote from "./AddNote"
+import { Row, Col } from "react-bootstrap"
 
 const NotesList = ({ notes, handleAddNote, handleDeleteNote }) => {
   return (
     <div>
-      {notes.map((note) => (
-        <Note
-          key={note.id}
-          id={note.id}
-          text={note.text}
-          date={note.date}
-          handleDeleteNote={handleDeleteNote}
-        />
-      ))}
+      <Row xs={1}>
+        {notes.map((note) => (
+          <Col className="col-md-4" key={note.id}>
+            <Note
+              key={note.id}
+              id={note.id}
+              text={note.text}
+              date={note.date}
+              handleDeleteNote={handleDeleteNote}
+            />
+          </Col>
+        ))}
+      </Row>
       <AddNote handleAddNote={handleAddNote} />
     </div>
   )
 }
 
 export default NotesList
+
+// prev:
+
+// return (
+//   <div>
+//     {notes.map((note) => (
+//       <Note
+//         key={note.id}
+//         id={note.id}
+//         text={note.text}
+//         date={note.date}
+//         handleDeleteNote={handleDeleteNote}
+//       />
+//     ))}
+//     <AddNote handleAddNote={handleAddNote} />
+//   </div>
+// )
