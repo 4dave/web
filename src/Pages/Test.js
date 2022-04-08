0a825f4c781ac0ce4b54af423d12d8ee1b6f9581
Dave Fredkove <david.fredkove@target.com>
2022-04-08 11:10:58 -0500

    added summary/details elements example

--- src/Pages/Test.js
+++ src/Pages/Test.js
@@ -2,24 +2,34 @@ import React from "react"
 
 const Test = () => {
   return (
-    <div className="page-container">
-      <h1>This is a test page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi id, eos
-        consequatur odit ab animi ad voluptates aliquid veritatis nam hic
-        laboriosam reprehenderit sed soluta. Odio corrupti sequi repellendus
-        vero? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Praesentium corrupti sed tempora enim, molestias nulla vero! Aliquid
-        dicta ipsum vitae molestias reiciendis nobis ad possimus! Debitis veniam
-        similique voluptate. Aperiam. Lorem ipsum, dolor sit amet consectetur
-        adipisicing elit. In, labore illo reprehenderit architecto quae
-        voluptatum fugiat dolor hic non. Dolorum numquam ipsa expedita, vero
-        explicabo nemo repellendus facilis atque distinctio. Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Quisquam asperiores amet
-        laboriosam autem nisi impedit, vel dolor placeat deserunt animi hic
-        eveniet facere culpa sed! Nihil quos fuga delectus praesentium?
-      </p>
-    </div>
+    <>
+      <div className="page-container">
+        <h1>Hey 👋 this is Dave's sandbox</h1>
+        <div className="code-container">
+          <details>
+            <summary>&#9660;&#9650;Show</summary>
+            Just testing the new details and summary elements! <br />
+            <code>
+              <pre>
+                {`
+<details>
+  <summary>Details</summary>
+  put some content here 👍
+</details>`}
+              </pre>
+            </code>
+          </details>
+        </div>
+      </div>
+
+      <div className="code-container">
+        <details>
+          <summary>&#9660;&#9650;Expand this</summary>
+          Another example of a details/summary expandable element! <br />
+          <code>👍</code>
+        </details>
+      </div>
+    </>
   )
 }
 
